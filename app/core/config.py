1377599195fcea80814f9c1db4330430ab847e3d
sejeonglee <sejeonglee2@gmail.com>
2022-07-10 20:30:50 +0900

    Replace mysqlclient to mysql/connector

--- app/core/config.py
+++ app/core/config.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     # 60 minutes * 24 hours * 7 days = 7 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
 
     # Database connection
-    HOST: str = os.getenv('HOST')
-    PORT: int = os.getenv('PORT')
-    USERNAME: str = os.getenv('USERNAME')
-    PASSWORD: str = os.getenv('PASSWORD')
-    DATABASE: str = os.getenv('DATABASE')
-    FIRST_SUPERUSER_EMAIL: EmailStr = os.getenv('FIRST_SUPERUSER_EMAIL')
-    FIRST_SUPERUSER_PASSWORD: str = os.getenv('FIRST_SUPERUSER_PASSWORD')
-    SQLALCHEMY_DATABASE_URL = f'mysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+    HOST: str = os.getenv("HOST")
+    PORT: int = os.getenv("PORT")
+    USERNAME: str = os.getenv("USERNAME")
+    PASSWORD: str = os.getenv("PASSWORD")
+    DATABASE: str = os.getenv("DATABASE")
+    FIRST_SUPERUSER_EMAIL: EmailStr = os.getenv("FIRST_SUPERUSER_EMAIL")
+    FIRST_SUPERUSER_PASSWORD: str = os.getenv("FIRST_SUPERUSER_PASSWORD")
+    SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
     class Config:
         case_sensitive = True
