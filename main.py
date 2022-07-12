ef1843f25c427b72ff47e5734421e00839106bbb
sejeonglee <sejeonglee2@gmail.com>
2022-07-12 14:20:36 +0900

    Disable CORS protection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api import api_router
 from app.core.config import settings
@@ -13,5 +14,13 @@ def init() -> None:
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix=settings.API_V1_STR)
 init()
