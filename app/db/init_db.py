2ad895a23d23571c9fb7cb803f4ab3650b0ef756
sejeonglee <sejeonglee2@gmail.com>
2022-07-24 04:51:20 +0900

    Add new test data

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -10,16 +10,17 @@ from app.core.config import settings
 
 
 def init_db(db: Session) -> None:
-    user = crud_user.get_by_email(db, email=settings.FIRST_SUPERUSER_EMAIL)
-    if not user:
-        user_in = UserCreate(
-            email=settings.FIRST_SUPERUSER_EMAIL,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-            user_type=UserType.administrator,
-            name="이세정",
-        )
-        crud_user.create_superuser(db=db, obj_in=user_in)
+    init_test_db()
+    # user = crud_user.get_by_email(db, email=settings.FIRST_SUPERUSER_EMAIL)
+    # if not user:
+    #     user_in = UserCreate(
+    #         email=settings.FIRST_SUPERUSER_EMAIL,
+    #         password=settings.FIRST_SUPERUSER_PASSWORD,
+    #         is_superuser=True,
+    #         user_type=UserType.administrator,
+    #         name="이세정",
+    #     )
+    #     crud_user.create_superuser(db=db, obj_in=user_in)
 
 
 def init_test_db(db: Session) -> None:
